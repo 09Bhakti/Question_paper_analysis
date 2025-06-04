419d0573c29879abc1be6d3d48082cddfc478b2c
09Bhakti <132741211+09Bhakti@users.noreply.github.com>
2025-06-05 01:23:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from main import process_pdf
+from text_cleaning import clean_text
+from question_extraction import extract_questions
 
 st.title("📄 Question Paper Analyzer")
 
@@ -14,4 +16,4 @@ if uploaded_file:
 
     st.subheader("Extracted Questions:")
     for i, q in enumerate(result, 1):
-        st.markdown(f"**Q{i}.** {q}")
+        st.markdown(f"**{q}**")
